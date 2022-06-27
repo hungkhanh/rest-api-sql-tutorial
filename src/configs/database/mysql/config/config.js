c89463dda6ac376ecd55a78a52be7e447efb1fb9
hungpk <hungpk1412@gmail.com>
2022-06-27 18:45:43 +0700

    create table office & employee

---
+++ src/configs/database/mysql/config/config.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: 'root',
+    password: 'password',
+    database: 'myDB',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  // development: {
+  //   username: process.env.MYSQL_USER,
+  //   password: process.env.MYSQL_PASSWORD,
+  //   database: process.env.MYSQL_DB,
+  //   host: process.env.MYSQL_HOST,
+  //   dialect: 'mysql',
+  // },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+  }
+}
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": "password",
+//     "database": "myDB",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
---
+++ src/configs/database/mysql/migrations/20220627081829-create-tables.js
@@ -0,0 +1,113 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    // Office
+    await queryInterface.createTable('office', 
+      {
+        id: {
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4,
+          primaryKey: true,
+        },
+        city: {
+          type: Sequelize.STRING,
+        },
+        phone: {
+          type: Sequelize.STRING,
+        },
+        addressLine1: {
+          type: Sequelize.STRING,
+        },
+        addressLine2: {
+          type: Sequelize.STRING,
+        },
+        state: {
+          type: Sequelize.STRING,
+        },
+        country: {
+          type: Sequelize.STRING,
+        },
+        postalCode: {
+          type: Sequelize.STRING,
+        },
+        territory: {
+          type: Sequelize.STRING,
+        },
+        createBy: {
+          type: Sequelize.UUID,
+        },
+        updateBy: {
+          type: Sequelize.UUID,
+        },
+      },
+      {
+        timestamps: true,
+        createAt: true,
+        updateAt: true,
+      }
+    )
+
+    // Employee
+    await queryInterface.createTable('employee', 
+      {
+        id: {
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4,
+          primaryKey: true,
+        },
+        lastName: {
+          type: Sequelize.STRING,
+        },
+        firstName: {
+          type: Sequelize.STRING,
+        },
+        extension: {
+          type: Sequelize.STRING,
+        },
+        email: {
+          type: Sequelize.STRING,
+        },
+        officeId: {
+          type: Sequelize.UUID,
+        },
+        reportsTo: {
+          type: Sequelize.UUID,
+        },
+        jobTitle: {
+          type: Sequelize.STRING,
+        },
+        createBy: {
+          type: Sequelize.UUID,
+        },
+        updateBy: {
+          type: Sequelize.UUID,
+        },
+      },
+      {
+        timestamps: true,
+        createAt: true,
+        updateAt: true,
+      }
+    )
+
+    await queryInterface.addConstraint('employee',
+      {
+        name: 'FK_employee_office',
+        fields: ['officeId'],
+        type: 'foreign key',
+        references: {
+          table: 'office',
+          field: 'id',
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      }
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('office');
+    await queryInterface.dropTable('employee');
+  }
+};
